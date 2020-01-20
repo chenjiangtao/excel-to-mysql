7832e3089ed1e40e78de38bf0c9c45a10bbb2dae
Aaron <evane1890@gmail.com>
2020-01-20 17:23:13 +0800

    完善代码

--- src/main/java/com/topstream/utils/Excel2SQL.java
+++ src/main/java/com/topstream/utils/Excel2SQL.java
@@ -1,131 +1,19 @@
 package com.topstream.utils;
 
+import com.alibaba.fastjson.JSONObject;
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Excel2SQL {
 
-/*表样例：
-    DROP TABLE IF EXISTS `t_table1`;
-    CREATE TABLE `t_table1` (
-       `id` int NOT NULL AUTO_INCREMENT COLLATE utf8_bin COMMENT '记录唯一ID；说明：记录唯一ID；样例：1234567',
-       `name` varchar(10) NOT NULL COLLATE utf8_bin COMMENT '名字；样例：张三',
-       `age` int(5) NOT NULL COLLATE utf8_bin COMMENT '年龄；说明：小于150；样例：30',
-       `import_time` datetime  DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COLLATE utf8_bin COMMENT '数据插入时间',
-    PRIMARY KEY (`id`)
-    ) ENGINE = InnoDB
-    DEFAULT CHARACTER SET = utf8
-    COLLATE = utf8_bin
-    COMMENT = '表1'
-    ROW_FORMAT = COMPACT;
-    */
-
-
-    public static void printSQL(String filePath) {
-        File file = new File(filePath); //根据文件path获取文件
-        List sheelList = Excel2SQL.readExcel(file);
-        //System.out.println("list中的数据打印出来");
-
-        System.out.println("\nSET NAMES utf8mb4;\n" +
-                "SET FOREIGN_KEY_CHECKS = 0;");
-
-
-        for (int i = 0; i < sheelList.size(); i++) {
-            List rowList = (List) sheelList.get(i);
-            StringBuffer table = new StringBuffer();
-
-            StringBuffer rows = new StringBuffer();
-            StringBuffer insertC = new StringBuffer();
-            StringBuffer insertV = new StringBuffer();
-
-
-            int startRow = 3;
-            int tableNameRow = 0;
-
-
-            for (int j = startRow; j < rowList.size(); j++) {
-
-                List columList = (List) rowList.get(j);
-
-                int columStart, columEnd;
-                if (rowList.size() >= 3) {
-                    columStart = 1;//起始列
-                    columEnd = 3;//结束列
-                } else {
-                    columStart = 0;//起始列
-                    columEnd = rowList.size();//结束列
-                }
-
-
-                //在excel中做固定，不要在程序中选择
-                //第一列：列名，第二列类型，第三列描述....
-                //字段ID	数据类型	可空	默认值	字段名称	字段补充说明	数据样例
-                String clnName = columList.get(columStart + 1).toString().trim();//字段ID
-                String type = columList.get(columStart + 2).toString().trim();//数据类型
-                String isNull = columList.get(columStart + 3).toString().trim();//可空
-                String comment = columList.get(columStart + 5).toString().trim();//字段名（中文名称）
-                String comment1 = columList.get(columStart + 6).toString().trim();//字段补充说明
-                String comment2 = columList.get(columStart + 7).toString().trim();//数据样例
-                comment = comment.replaceAll("'", "\"");
-
-
-                rows.append("   `").append(clnName).append("` ")//cln name
-                        .append(type)//类型
-                        .append(" " + isNull)//是否非空
-                        //`import_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '数据插入时间'
-                        .append(clnName.equals("import_time") ? " DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP" : "")
-                        .append(clnName.equals("id") ? " AUTO_INCREMENT" : "")
-                        .append(" COLLATE utf8_bin COMMENT ")
-                        .append("'")//注释
-                        .append(comment)//字段名（中文名称）
-                        .append(comment1 == null || comment1.trim().equals("") ? "" : "；说明：" + comment1)//字段补充说明
-                        .append(comment2 == null || comment2.trim().equals("") ? "" : "；样例：" + comment2)//数据样例
-                        .append("',\n");
-
-                //组将insert
-                if (!clnName.equals("id") && !clnName.equals("import_time")) {
-                    insertC.append(clnName)
-                            .append(",");
-
-                    insertV.append(type.contains("int") ? "" : "\"")
-                            .append(type.contains("int") && !clnName.equals("id") ? 1 : "")
-                            .append(type.contains("char") ? "testval" + j : "")
-                            .append(type.contains("date") && !clnName.equals("import_time") ? "2019-01-16" : "")
-                            .append(type.contains("int") ? "" : "\"")
-                            .append(",");
-                }
-            }
-
-            //表名、主键等
-            String pk = ((List) rowList.get(startRow)).get(2).toString().trim();
-            String tableName = ((List) rowList.get(tableNameRow)).get(1).toString().trim();
-            String tableComment = ((List) rowList.get(tableNameRow)).get(9).toString().trim();
-
-            table.append("\nDROP TABLE IF EXISTS `" + tableName + "`;\nCREATE TABLE `" + tableName + "` (\n");
-            table.append(rows.toString().substring(0, rows.length() - 1) + "\n");
-            table.append("PRIMARY KEY (`" + pk + "`)\n) ");
-            table.append("ENGINE = InnoDB\n" +
-                    "DEFAULT CHARACTER SET = utf8\n" +
-                    "COLLATE = utf8_bin\n");
-            table.append("COMMENT = '" + tableComment + "'\n" +
-                    "ROW_FORMAT = COMPACT;");
-
-            //打印建表语句
-            System.out.println(table);
-
-            //insert
-            String replace = insertV.toString().replace("\"\"", "");
-            String ins = "insert into " + tableName + " (" + insertC.substring(0, insertC.length() - 1) + ")values(" + replace.substring(0, replace.length() - 1) + ")";
-            //打印插入语句
-            //System.out.println(ins);
-
-        }
-    }
+    private static String fixStr = "char(";
 
     // 去读Excel的方法readExcel，该方法的入口参数为一个File对象
     public static List readExcel(File file) {
@@ -175,6 +63,188 @@ public class Excel2SQL {
         }
 
         return null;
+    }
+
+/*表样例：
+    DROP TABLE IF EXISTS `t_table1`;
+    CREATE TABLE `t_table1` (
+       `id` int NOT NULL AUTO_INCREMENT COLLATE utf8_bin COMMENT '记录唯一ID；说明：记录唯一ID；样例：1234567',
+       `name` varchar(10) NOT NULL COLLATE utf8_bin COMMENT '名字；样例：张三',
+       `age` int(5) NOT NULL COLLATE utf8_bin COMMENT '年龄；说明：小于150；样例：30',
+       `import_time` datetime  DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COLLATE utf8_bin COMMENT '数据插入时间',
+    PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8
+    COLLATE = utf8_bin
+    COMMENT = '表1'
+    ROW_FORMAT = COMPACT;
+    */
+
+
+    public static void printSQL(String filePath) {
+        File file = new File(filePath); //根据文件path获取文件
+        List sheelList = Excel2SQL.readExcel(file);
+        //System.out.println("list中的数据打印出来");
+
+        System.out.println("\nSET NAMES utf8mb4;\n" +
+                "SET FOREIGN_KEY_CHECKS = 0;");
+
+        Map<String, String> clnCommentMap = new HashMap();
+
+
+        for (int i = 0; i < sheelList.size(); i++) {
+            StringBuffer table = new StringBuffer();
+            StringBuffer rows = new StringBuffer();
+            StringBuffer insertC = new StringBuffer();
+            StringBuffer insertV = new StringBuffer();
+            StringBuilder nullMap = new StringBuilder();
+            int startRow = 3;
+            int tableNameRow = 0;
+
+            List rowList = (List) sheelList.get(i);
+
+            //表名、主键等
+            String pk = ((List) rowList.get(startRow)).get(2).toString().trim();
+            String tableName = ((List) rowList.get(tableNameRow)).get(1).toString().trim();
+            String tableComment = ((List) rowList.get(tableNameRow)).get(9).toString().trim();
+
+            for (int j = startRow; j < rowList.size(); j++) {
+
+                List columList = (List) rowList.get(j);
+
+                int columStart, columEnd;
+                if (rowList.size() >= 3) {
+                    columStart = 1;//起始列
+                    columEnd = 3;//结束列
+                } else {
+                    columStart = 0;//起始列
+                    columEnd = rowList.size();//结束列
+                }
+
+
+                //在excel中做固定，不要在程序中选择
+                //第一列：列名，第二列类型，第三列描述....
+                //字段ID	数据类型	可空	默认值	字段名称	字段补充说明	数据样例
+                String clnName = columList.get(columStart + 1).toString().trim();//字段ID
+                String type = columList.get(columStart + 2).toString().trim();//数据类型
+                String isNull = columList.get(columStart + 3).toString().trim();//可空
+                String comment = columList.get(columStart + 5).toString().trim();//字段名（中文名称）
+                String comment1 = columList.get(columStart + 6).toString().trim();//字段补充说明
+                String comment2 = columList.get(columStart + 7).toString().trim();//数据样例
+                comment = comment.replaceAll("'", "\"");
+
+                clnCommentMap.put(clnName, comment + ":" + comment1);//记录、去重
+
+                rows.append("   `").append(clnName).append("` ")//cln name
+                        .append(type)//类型
+                        .append(" " + isNull)//是否非空
+                        //`import_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '数据插入时间'
+                        .append(clnName.equals("import_time") ? " DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP" : "")
+                        .append(clnName.equals("id") ? " AUTO_INCREMENT" : "")
+                        .append(" COLLATE utf8_bin COMMENT ")
+                        .append("'")//注释
+                        .append(comment)//字段名（中文名称）
+                        .append(comment1 == null || comment1.trim().equals("") ? "" : "；说明：" + comment1)//字段补充说明
+                        .append(comment2 == null || comment2.trim().equals("") ? "" : "；样例：" + comment2)//数据样例
+                        .append("',\n");
+
+                //组将insert
+                //if (!clnName.equals("id")
+                //&& !clnName.equals("import_batch_no")
+                //&& !clnName.equals("import_time")) { //跳过id,import_batch_no，import_time
+                insertC.append(clnName)
+                        .append(",");
+                insertV.append(type.contains("int") ? "" : "\"")
+                        .append(type.contains("int") ? 0 : "")
+                        .append(type.contains("char") ? "testval" + j : "")
+                        .append(type.contains("date") ? "2019-01-16" : "")
+                        .append(type.contains("int") ? "" : "\"")
+                        .append(",");
+                //t_card_info.put("l_card_no", 0);
+                if (!isNull.equals("NOT NULL")) {
+                    nullMap.append(tableName + ".put(\"" + clnName + "\", 0);\n");
+                }
+                //}
+
+
+            }
+
+
+            table.append("\nDROP TABLE IF EXISTS `" + tableName + "`;\nCREATE TABLE `" + tableName + "` (\n");
+            table.append(rows.toString().substring(0, rows.length() - 1) + "\n");
+            table.append("PRIMARY KEY (`" + pk + "`)\n) ");
+            table.append("ENGINE = InnoDB\n" +
+                    "DEFAULT CHARACTER SET = utf8\n" +
+                    "COLLATE = utf8_bin\n");
+            table.append("COMMENT = '" + tableComment + "'\n" +
+                    "ROW_FORMAT = COMPACT;");
+
+
+            //******************打印建表语句******************
+            //System.out.println(table);
+
+
+            //******************打印插入语句******************
+            String replace = insertV.toString().replace("\"\"", "");
+            //String ins = "insert into " + tableName + " (" + insertC.substring(0, insertC.length() - 1) + ")values(" + replace.substring(0, replace.length() - 1) + ");";
+            //System.out.println(ins);
+
+            //******************打印CleanlingConf类的字段******************
+            //System.out.println("private static String " + tableName + "_clns=\"" + insertC.substring(0, insertC.length() - 1) + "\";");
+            //System.out.println(nullMap);
+
+
+        }
+        //叠加的，所以放外面
+        //******************打印CleanlingConf类的字段+描述******************
+        System.out.println(JSONObject.toJSON(clnCommentMap));
+
+    }
+
+    /**
+     * ***********如果都是utf-8类型的，不需要转换***********
+     * varchar、char字段长度增长1.3倍，适应hive
+     *
+     * @param table
+     * @return
+     */
+    private static String sql4Hive(String table) {
+        String s = " DROP TABLE IF EXISTS `t_table1`;\n" +
+                "    CREATE TABLE `t_table1` (\n" +
+                "       `id` int NOT NULL AUTO_INCREMENT COLLATE utf8_bin COMMENT '记录唯一ID；说明：记录唯一ID；样例：1234567',\n" +
+                "       `name` varchar(10) NOT NULL COLLATE utf8_bin COMMENT '名字；样例：张三',\n" +
+                "       `age` int(5) NOT NULL COLLATE utf8_bin COMMENT '年龄；说明：小于150；样例：30',\n" +
+                "       `import_time` datetime  DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COLLATE utf8_bin COMMENT '数据插入时间',\n" +
+                "    PRIMARY KEY (`id`)\n" +
+                "    ) ENGINE = InnoDB\n" +
+                "    DEFAULT CHARACTER SET = utf8\n" +
+                "    COLLATE = utf8_bin\n" +
+                "    COMMENT = '表1'\n" +
+                "    ROW_FORMAT = COMPACT;";
+
+        String[] split = s.split("\n");
+        for (String sp : split) {
+            String tmp = sp.split(" ")[1];
+            String rpstr = s.substring(s.indexOf(fixStr) + fixStr.length(), s.indexOf(") "));
+            int lens = Integer.valueOf(rpstr);
+
+            int tl = (int) (lens * 1.3);
+
+
+            System.out.println(tmp);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        //sql4Hive("");
+        String s = "varchar(50) NOT NULL COLLATE utf8_bin COMMENT '名字；样例：张三'";
+
+        String substring = s.substring(s.indexOf(fixStr) + fixStr.length(), s.indexOf(") "));
+        System.out.println(substring);
+        int tl = (int) (Integer.valueOf(substring) * 1.35);
+        System.out.println(tl);
+
     }
 
 
--- src/test/java/com/topstream/utils/ExcelTest.java
+++ src/test/java/com/topstream/utils/ExcelTest.java
@@ -1,10 +1,13 @@
 package com.topstream.utils;
 
+import com.alibaba.fastjson.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class ExcelTest {
@@ -12,7 +15,28 @@ class ExcelTest {
     @Test
     void contextLoads() {
         //Excel2SQL.printSQL("doc/template.xls");
-        Excel2SQL.printSQL("doc/excampleFile.xls");
+        Excel2SQL.printSQL("doc/template.xls");
+    }
+
+    @Test
+    void test() {
+
+        //
+        //Map<String, Object> testMap = new HashMap<String, Object>();
+        //testMap.put("a", "b");
+        //testMap.put("a", "b");
+        //testMap.put("a", "b");
+        //
+        //String o = JSONObject.toJSONString(testMap);
+        //System.out.println(o);
+
+        String json = "{\"recharge_amount\":\"累计充值金额:\",\"init_card_status\":\"初始状态:4:未激活 6:激活\",\"account_type\":\"账户类型:URP\",\"after_valid_end_date\":\"交易后有效期结束日期:\",\"partner_name\":\"交易商户名称:\",\"cashregister_no\":\"收银机号:URP\",\"l_card_no\":\"长卡号:\",\"import_batch_no\":\"导入批次:无需三家填写\",\"order_status\":\"交易状态:0:等待、1:成功、2:失败…\",\"after_valid_end_time\":\"交易后有效期结束时间:\",\"is_repeat_b\":\"卡能否重复消费:\",\"province\":\"省份:URP\",\"is_repeat_a\":\"卡能否重复充值:\",\"associated_order_no\":\"关联订单号:\",\"valid_end_time\":\"有效期结束时间:\",\"net_name\":\"NETNAME:URP\",\"id\":\"ID:记录唯一ID\",\"import_data_source\":\"数据来源（1:PNC，2:FEEMUD，3:E-BUY）:\",\"a_account\":\"A账户:URP\",\"change_item\":\"变更项:变更的字段名：有效期、卡状态\",\"auth_code\":\"授权码:URP\",\"area_no\":\"区域编号:URP\",\"is_replace\":\"是否补卡过:\",\"company_name\":\"公司名称:\",\"rloss_card_time\":\"挂失时间:\",\"before_valid_start_date\":\"交易前有效期开始日期:\",\"card_name\":\"卡名称:\",\"l_expired_time\":\"长码过期时间:\",\"after_amount_a\":\"交易后余额:\",\"after_amount_b\":\"交易后补贴余额:\",\"trans_id\":\"交易号/订单号:\",\"b_change_value\":\"变更前值:\",\"old_card_no\":\"旧卡号:URP\",\"swap_type\":\"交易类型:类型ID +“-” +名称\",\"freeze_card_date\":\"冻结日期:\",\"update_time\":\"更新时间:\",\"card_no\":\"卡号:\",\"freeze_amount_b\":\"冻结补贴金额:\",\"freeze_amount_a\":\"冻结金额:\",\"enc_type\":\"长卡号加密方式:\",\"org_name\":\"机构名称:URP\",\"unfreeze_card_date\":\"解冻日期:\",\"channel_name\":\"发卡渠道:\",\"apply_type1\":\"卡类型1:类型ID +“-” +名称\",\"apply_type2\":\"卡类型2:类型ID +“-” +名称\",\"create_card_date\":\"生卡日期:\",\"result_info\":\"结果信息:成功、失败\",\"card_type\":\"卡类型:URP\",\"rloss_card_date\":\"挂失日期:\",\"activate_date\":\"激活日期:URP\",\"sku_no\":\"sku:卡品类号\",\"settle_amount\":\"清算金额:URP\",\"freeze_card_time\":\"冻结时间:\",\"init_amount_a\":\"激活金额:指初始金额\",\"init_amount_b\":\"补贴金额:指初始折扣金额\",\"valid_start_time\":\"有效期开始时间:\",\"recharge_type\":\"充值类型:线上APP充值、门店充值\",\"before_valid_end_date\":\"交易前有效期结束日期:\",\"store_no\":\"交易门店号:\",\"activate_time\":\"激活时间:URP\",\"track_no\":\"外部流水号:URP\",\"active_card_date\":\"激活日期:\",\"partner_code\":\"交易商户号:\",\"delete_flag\":\"删除标识:逻辑删除标识\\n(0:正常、1:删除)\",\"create_date\":\"创建日期:\",\"first_trans_no\":\"首笔交易交易号:首笔交易订单号\",\"payment_method\":\"支付方式:URP\",\"consume_amount\":\"累计消费金额:\",\"before_amount_a\":\"交易前余额:\",\"activation_name\":\"激活门店:URP\",\"before_amount_b\":\"交易前补贴余额:\",\"create_time\":\"创建时间:\",\"opt_result\":\"操作结果:成功、失败\",\"sales_no\":\"销售单号:URP\",\"invalid_card_date\":\"作废日期:\",\"import_time\":\"数据插入时间:无需三家填写\",\"valid_start_date\":\"有效期开始日期:\",\"recharge_trans_type\":\"充值交易类型:微信充值、支付宝充值等\",\"l_expired_date\":\"长码过期日期:\",\"product_name\":\"产品名称:URP\",\"update_date\":\"更新日期:\",\"is_used\":\"是否使用过:\",\"unfreeze_card_time\":\"解冻时间:\",\"create_card_time\":\"生卡时间:制卡时间\",\"active_card_time\":\"激活时间:\",\"discount_scale\":\"贴现率:URP\",\"net_code\":\"NETCODE:URP\",\"outer_trade_id\":\"关联外部订单号:第三方订单号\",\"o_card_no\":\"原卡号:\",\"after_valid_start_date\":\"交易后有效期开始日期:\",\"card_amount_b\":\"补贴金额:指折扣金额\",\"b_account\":\"B账户:URP\",\"card_amount_a\":\"金额:\",\"trans_amount_a\":\"交易金额:\",\"closs_card_time\":\"解挂时间:\",\"import_batch_date\":\"导入批次日期(格式为：YYYYMMDD):\",\"first_recharge_no\":\"首笔充值交易号:首笔交易订单号\",\"swap_time\":\"交易时间:\",\"activation_channel\":\"激活渠道:URP\",\"s_card_no\":\"短卡号:\",\"issuing_bank\":\"发卡行:URP\",\"store_name\":\"交易门店名称:\",\"unactive_card_date\":\"取消激活日期:\",\"batch_no\":\"批次号:\",\"closs_card_date\":\"解挂日期:\",\"after_valid_start_time\":\"交易后有效期开始时间:\",\"pos_serial_no\":\"Pos流水号:\",\"before_valid_start_time\":\"交易前有效期开始时间:\",\"before_valid_end_time\":\"交易前有效期结束时间:\",\"card_status\":\"卡状态:类型ID +“-” +名称\",\"invalid_card_time\":\"作废时间:\",\"valid_end_date\":\"有效期结束日期:\",\"service_charge\":\"手续费:URP\",\"unactive_card_time\":\"取消激活时间:\",\"swap_date\":\"交易日期:\",\"a_change_value\":\"变更后值:\",\"pos_no\":\"POS号:\"}\n";
+        JSONObject jsonObject = JSONObject.parseObject(json);
+        Map<String, Object> valueMap = new HashMap<String, Object>();
+        valueMap.putAll(jsonObject);
+        System.out.println(valueMap.get("recharge_amount"));
+
+
     }
 
 }
